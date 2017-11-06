a74446b31824be3d1e93bff76c8f5095b78d4c62
Chestnut27 <33204695+Chestnut27@users.noreply.github.com>
2017-11-05 18:33:46 -0600

    Update jobs.js

--- jobs.js
+++ jobs.js
@@ -10,7 +10,7 @@ function myFunction() {
   // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
 	td = tr[i].getElementsByTagName("ZIP")[0];
-    if (td) {
+  } if (td) {
       if (td.innerHTML.indexOf(filter) > -1) {
     	tr[i].style.display = "";
   	} else {
