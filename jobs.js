6fa82be18c0fa090d4591c932a7c997a9bc8d32f
Chestnut27 <33204695+Chestnut27@users.noreply.github.com>
2017-11-05 20:16:22 -0600

    Update jobs.js

--- jobs.js
+++ jobs.js
@@ -1,17 +1,15 @@
-<script>
 function sortByZIP() {
   // Declare variables 
-  var input, filter, zipfilter, tr, td, i;
+  var input, filter, zipfilter, tr, td, i, edFilter;
   input = document.getElementById("ZIP");
   filter = input.value();
   zipfilter = document.getElementById("ZIP");
-  
 
 	
 	
 	
 	
-  // Loop through all table rows, and hide those who don't match the search query
+  // Loop through all table rows, and hide those who don't match the search query 
   for (i = 0; i < tr.length; i++) {
 	td = tr[i].getElementsByTagName("ZIP")[0];
   } if (td) {
@@ -20,22 +18,17 @@ function sortByZIP() {
   	} else {
     	tr[i].style.display = "none";
   	}
-    } 
   }
 }
 
-function removezipfilter(){
-	
-	
-	
+function removeZipFilter(){
+  document.getElementById("zip-input").value = "";
 }
 
-function removejobfilter(){
+function removeJobFilter(){
+    document.getElementById("job-type-select").value = "job_type";
 }
 
-function removeeducationfilter(){
+function removeEducationFilter() {
+  document.getElementById("education-select").value = "education";
 }
-
-
-
-</script>
