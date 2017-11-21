f40ddb7fbae8ec00e320e1d3a60ca9b54c047902
amergner <33204545+amergner@users.noreply.github.com>
2017-11-21 00:22:37 -0600

    Add files via upload

--- jobs.js
+++ jobs.js
@@ -1,5 +1,8 @@
+var text;
+
 var jobTypeSelect = document.getElementById("jobTypeSelect");
 var educationTypeSelect = document.getElementById("educationTypeSelect");
+var zipNumSelect = document.getElementById("zipInput");
 
 var jobs = document.getElementsByClassName('job');
 var constructionjobs = document.getElementsByClassName("Construction");
@@ -11,7 +14,6 @@ var maintenancejobs = document.getElementsByClassName("Maintenance");
 var officeworkjobs = document.getElementsByClassName("OfficeWork");
 var onlineemploymentjobs = document.getElementsByClassName("OnlineEmployment");
 
-
 var highschooljobs=document.getElementsByClassName("Highschool");
 var diplomajobs=document.getElementsByClassName("Diploma");
 var collegejobs=document.getElementsByClassName("College");
@@ -20,8 +22,23 @@ function findJob() {
   showAll();
   for(var i = 0; i < jobs.length; i++) {
     if(!((jobs[i].classList.contains(jobTypeSelect.value) || jobTypeSelect.value === "job_type")
-     && (jobs[i].classList.contains(educationTypeSelect.value) || educationTypeSelect.value === "education")))
+     && (jobs[i].classList.contains(educationTypeSelect.value) || educationTypeSelect.value === "education") 
+     && (jobs[i].classList.contains(zipNumSelect.value) || zipNumSelect.value === "")))
       jobs[i].style.display="none";
+  }
+}
+
+zipInput.onkeypress = function(e) {
+  var key = e.which;
+  if(e == 13) {
+    findJob();
+  }
+}
+
+zip_landing.onkeypress = function(e) {
+  var key = e.which;
+  if(e == 13) {
+    zipNumSelect.value = getElementById("zip_landing");
   }
 }
 
